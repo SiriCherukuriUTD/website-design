8a3a3f04d21d13678fd47d5d08b774004efe1393
SiriCherukuriUTD <sxc230129@utdallas.edu>
2026-02-21 22:26:49 +0000

    coca cola waste pollution

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
-// main.js
+console.log("Hello from main.js");
 
-console.log("Hello, World!");
+let targetElement = document.querySelector("h2");
+
+console.log(targetElement.outerHTML);
+
+targetElement.addEventListener("mouseover", function () {
+	console.log(this.innerText);
+	this.innerText = "WASTE";
+	this.style = "cursor: wait";
+});
+
+targetElement.addEventListener("mouseout", function () {
+	this.innerText = "Coca Cola";
+	this.style = "cursor: pointer";
+});
